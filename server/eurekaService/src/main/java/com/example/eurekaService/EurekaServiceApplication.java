50b7f2f97dc1b953d9e7b56779298aa827d97a96
IvanaBlagojevic <ivana.blagojevic@uns.ac.rs>
2019-12-05 21:09:55 +0100

    arhitektura korekcije

--- server/eurekaService/src/main/java/com/example/eurekaService/EurekaServiceApplication.java
+++ server/eurekaService/src/main/java/com/example/eurekaService/EurekaServiceApplication.java
@@ -1,5 +1,8 @@
 package com.example.eurekaService;
 
+
+import org.hibernate.validator.internal.util.logging.LoggerFactory;
+import org.jboss.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -7,6 +10,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceApplication {
+	
 
 	public static void main(String[] args) {
 		System.setProperty("KEY_STORE_CLASSPATH", "src/main/resources/eureka.jks");
@@ -16,6 +20,8 @@ public class EurekaServiceApplication {
 		System.setProperty("KEY_STORE_ALIAS", "eureka");
 		
 		SpringApplication.run(EurekaServiceApplication.class, args);
+		System.out.print("EurekaService start");
+		
 	}
 
 }
--- server/payPalService/src/main/java/com/example/payPalService/PayPalServiceApplication.java
+++ server/payPalService/src/main/java/com/example/payPalService/PayPalServiceApplication.java
@@ -1,45 +1,41 @@
-package com.example.kpService;
+package com.example.payPalService;
 
 import java.security.NoSuchAlgorithmException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import com.netflix.discovery.DiscoveryClient;
 import com.netflix.discovery.shared.transport.jersey.EurekaJerseyClientImpl.EurekaJerseyClientBuilder;
 
-@RefreshScope
 @EnableEurekaClient
 @SpringBootApplication
-public class KpServiceApplication {
+public class PayPalServiceApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("javax.net.ssl.trustStore","src/main/resources/kpTrusted.jks");
+		System.setProperty("javax.net.ssl.trustStore","src/main/resources/payTrusted.jks");
         System.setProperty("javax.net.ssl.trustStorePassword","password");
 		
-		
-		System.setProperty("KEY_STORE_CLASSPATH", "src/main/resources/kp.jks");
-		System.setProperty("KEY_STORE_CLASSPATH_TRUST", "src/main/resources/kpTrusted.jks");
+		System.setProperty("KEY_STORE_CLASSPATH", "src/main/resources/pay.jks");
+		System.setProperty("KEY_STORE_CLASSPATH_TRUST", "src/main/resources/payTrusted.jks");
 		System.setProperty("KEY_STORE_PASSWORD", "password");
 		System.setProperty("KEY_STORE_TRUST_PASSWORD", "password");
-		System.setProperty("KEY_STORE_ALIAS", "kp");
+		System.setProperty("KEY_STORE_ALIAS", "pay");
 		
-		SpringApplication.run(KpServiceApplication.class, args);
+		SpringApplication.run(PayPalServiceApplication.class, args);
 	}
-	
 	
 	@Bean
 	public DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs() throws NoSuchAlgorithmException {
 		DiscoveryClient.DiscoveryClientOptionalArgs args = new DiscoveryClient.DiscoveryClientOptionalArgs();
-		System.setProperty("javax.net.ssl.keyStore", "src/main/resources/kp.jks");
+		System.setProperty("javax.net.ssl.keyStore", "src/main/resources/pay.jks");
 		System.setProperty("javax.net.ssl.keyStorePassword", "password");
-		System.setProperty("javax.net.ssl.trustStore", "src/main/resources/kpTrusted.jks");
+		System.setProperty("javax.net.ssl.trustStore", "src/main/resources/payTrusted.jks");
 		System.setProperty("javax.net.ssl.trustStorePassword", "password");
 		EurekaJerseyClientBuilder builder = new EurekaJerseyClientBuilder();
-		builder.withClientName("kp");
+		builder.withClientName("pay");
 		builder.withSystemSSLConfiguration();
 		builder.withMaxTotalConnections(10);
 		builder.withMaxConnectionsPerHost(10);
